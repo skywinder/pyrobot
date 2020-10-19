ec9cfd382acb81a1006178171c362743b8faf95b
Petr Korolev <sky4winder@gmail.com>
2020-10-19 16:44:50 +0700

    idea install venv

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# from pyrogram import Client
+#
+# with Client("my_account", api_id, api_hash) as app:
+#     app.send_message("me", "Greetings from **Pyrogram**!")
+
 from pyrogram import Client
 
-with Client("my_account", api_id, api_hash) as app:
-    app.send_message("me", "Greetings from **Pyrogram**!")
+app = Client("my_account")
+app.run()
